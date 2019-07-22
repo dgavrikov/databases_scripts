0cb1c970a395e37266cccd3800967bc4efe7460a
Dmitriy Gavrikov <dgavrikov84@gmail.com>
2019-07-22 23:53:50 +0300

    SQL Server add helpers SP for hotkeys

---
+++ SQL Server/helpers/sp_get_object_by_definition.sql
@@ -0,0 +1,36 @@
+﻿USE [master]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[sp_get_object_by_definition] 
+	@pattern nvarchar(max)
+as
+begin
+	/*
+		Author:	Dmitriy Gavrikov
+		Note:	for hotkey in SSMS
+	*/
+	declare @sql varchar(4000) =
+'select
+	 O.name					as [object_name]
+	,quotename(schema_name(O.schema_id))+''.''+quotename(O.name) 
+							as [full_name]
+	,O.object_id
+	,O.parent_object_id
+	,O.type
+	,O.type_desc
+	,O.create_date
+	,O.modify_date
+	,O.is_ms_shipped
+	,M.definition
+from
+	sys.all_objects O
+	inner join sys.sql_modules M on O.object_id = M.object_id
+where
+	M.definition like N''%'+@pattern + '%''
+order by
+	O.name';
+	exec(@sql);
+end
---
+++ SQL Server/helpers/sp_get_object_info_by_name.sql
@@ -0,0 +1,38 @@
+﻿USE [master]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER procedure [dbo].[sp_get_object_info_by_name] 
+	@object_name sysname
+as
+begin
+	/*
+		Author:	Dmitriy Gavrikov
+		Note:	for hotkey in SSMS
+	*/
+	declare @sql varchar(2000);
+
+	set @sql = '
+select
+	 O.name					as [object_name]
+	,quotename(schema_name(O.schema_id))+''.''+quotename(O.name) 
+							as [full_name]
+	,O.object_id
+	,O.parent_object_id
+	,O.type
+	,O.type_desc
+	,O.create_date
+	,O.modify_date
+	,O.is_ms_shipped
+	,M.definition
+from
+	sys.all_objects O
+	left join sys.sql_modules M on O.object_id = M.object_id
+where
+	name like N'''+@object_name + '%''
+order by
+	O.name';
+	exec (@sql);
+end
